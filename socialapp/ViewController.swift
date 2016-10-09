d4a52c747311844a690d7402a9cc2c74ccf2ad68
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-10-09 18:26:15 +0100

    Initial Commit

---
+++ socialapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  socialapp
+//
+//  Created by PH Soria on 09/10/2016.
+//  Copyright © 2016 Pierre-Henry Soria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
