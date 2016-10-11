d152c406f987738e6231a2a806df8b1eea569fcd
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-10-11 23:47:13 +0100

    Add other layer properties

--- socialapp/SignInView.swift
+++ socialapp/SignInView.swift
@@ -13,6 +13,9 @@ class SignInView: UIView {
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: SHADOW_GREY).cgColor
+        layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 0.5
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
     }
 }
